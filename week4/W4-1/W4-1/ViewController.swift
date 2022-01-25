084f538a917ab1ee694c2eae607fab89fb832950
莊凱富 <zhuangkaifu@zhuangkaifus-MacBook-Air.local>
2022-01-26 07:44:53 +0800

    addW4-1

---
+++ week4/W4-1/W4-1/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  W4-1
+//
+//  Created by 莊凱富 on 2022/1/26.
+//
+
+import UIKit
+
+class tenRowViewController: UITableViewController {
+
+    struct Row {
+        let title: String
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dfRow", for: indexPath)
+        let rowNumber : Int = indexPath.row
+        let row = Row (title: "This is section \(rowNumber), row \(rowNumber)")
+        cell.textLabel?.text = row.title
+        return cell
+    }
+}
+
