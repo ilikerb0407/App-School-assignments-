97c37b7133ad7063328b74be7f73f4506399e34f
ilikerb0407 <kevin12342247@gmail.com>
2022-03-28 13:40:18 +0800

    extend selectionViewDataSource

--- DI-part 1/DI-part 1/SelectionView.swift
+++ DI-part 1/DI-part 1/SelectionView.swift
@@ -7,7 +7,41 @@
 
 import UIKit
 
-class SelectionView: UIView {
+protocol SelectionViewDataSource {
+    func indicatorColor() -> UIColor
+    func numberOfButtons() -> Int
+    // 決定button的樣子
+    func selectionView() -> SelectionView.ButtonModel
+}
 
+// MARK: 用extension 才會有body可以寫,宣告default該長的樣子, README 第四點
+extension SelectionViewDataSource {
+    
+    func indicatorColor() -> UIColor {
+        return .blue
+    }
+    
+    func numberOfButtons() -> Int {
+        return 2
+    }
+    
+    func selectionView() -> SelectionView.ButtonModel {
+        return .init(title: "default", titleColor: .white, titleFont: .systemFont(ofSize: 18))
+    }
+}
+
+
+
+
+
+class SelectionView: UIView {
+    
+    struct ButtonModel {
+        let title: String
+        let titleColor: UIColor
+        let titleFont: UIFont
+    }
+
+    var dataSource: SelectionViewDataSource?
 
 }
